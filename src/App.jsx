63a92437a8b6509d363da32d6d395f36216ca0fa
Akshay V M <akshayvm36@gmail.com>
2024-04-02 21:52:45 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Componets/Header'
+import Slider from './Componets/Slider'
+import ProductionHouse from './Componets/ProductionHouse'
+import GenreMovieList from './Componets/GenreMovieList'
+import Banner from './Componets/Banner'
+import Footer from './Componets/Footer'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="">
+        <Header/>
+        
+        <Banner/>
+
+        <ProductionHouse/>
+
+        <GenreMovieList/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Componets/Banner.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import GlobalApi from "../Services/GlobalApi";
+import  { useRef } from "react";
+import './Banner.css'
+
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+  const elementRef = useRef();
+  useEffect(() => {
+    getTrendingMovies();
+  }, []);
+
+  const getTrendingMovies = () => {
+    GlobalApi.getTrendingVideos.then((resp) => {
+      console.log(resp.data.results);
+      var a = Math.floor(Math.random() * 10) + 1;
+
+      setMovie(resp.data.results[a]);
+    });
+  };
+  return (
+    <div
+      className="banner"
+      style={{
+        backgroundImage: `URL(${
+          movie ? IMAGE_BASE_URL + movie.poster_path : ""
+        })`,
+      }}
+    >
+      <div className="content">
+        <h1 className="title font-bold">{movie ? movie.title || movie.name : ""}</h1>
+        <div className="banner_buttons my-6 ">
+          <button className="button">Play</button>
+          <button className="button">My List</button>
+        </div>
+        <br />
+        <h1 className="description font-thin mt-2">{movie ? movie.overview : ""}</h1>
+      </div>
+      <div className="fade_bottom"></div>
+    </div>
+  );
+}
+
+export default Banner;
+
+//src={IMAGE_BASE_URL + item.backdrop_path}
