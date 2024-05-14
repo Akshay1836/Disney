f257cb1cd45427723af00c60b01ce82c26cf131e
Akshay V M <akshayvm36@gmail.com>
2024-05-14 19:41:13 +0530

    gei movies url chnaged 2

--- src/Componets/Banner.jsx
+++ src/Componets/Banner.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
-import GlobalApi from "../Services/GlobalApi";
-import  { useRef } from "react";
-import './Banner.css'
-
+import { YOUR_API_KEY_HERE } from "../../constants";
+import { useRef } from "react";
+import "./Banner.css";
+import axios from "axios";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 
@@ -13,8 +13,17 @@ function Banner() {
     getTrendingMovies();
   }, []);
 
+  const movieBaseUrl = "https://api.themoviedb.org/3";
+  const api_key = YOUR_API_KEY_HERE;
+
+  const getTrendingVideos = axios.get(
+    movieBaseUrl + "/trending/all/day?api_key=" + api_key
+  );
+  useEffect(() => {
+    getTrendingMovies();
+  }, []);
   const getTrendingMovies = () => {
-    GlobalApi.getTrendingVideos.then((resp) => {
+    getTrendingVideos.then((resp) => {
       console.log(resp.data.results);
       var a = Math.floor(Math.random() * 10) + 1;
 
@@ -31,13 +40,17 @@ function Banner() {
       }}
     >
       <div className="content">
-        <h1 className="title font-bold">{movie ? movie.title || movie.name : ""}</h1>
+        <h1 className="title font-bold">
+          {movie ? movie.title || movie.name : ""}
+        </h1>
         <div className="banner_buttons my-6 ">
           <button className="button">Play</button>
           <button className="button">My List</button>
         </div>
         <br />
-        <h1 className="description font-thin mt-2">{movie ? movie.overview : ""}</h1>
+        <h1 className="description font-thin mt-2">
+          {movie ? movie.overview : ""}
+        </h1>
       </div>
       <div className="fade_bottom"></div>
     </div>
