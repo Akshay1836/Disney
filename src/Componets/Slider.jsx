5aa24392f9353bbc06c46bb25a3b6f3d3d945492
Akshay V M <akshayvm36@gmail.com>
2024-05-14 18:37:03 +0530

    sliderfile issue changed

--- src/Componets/Slider.jsx
+++ src/Componets/Slider.jsx
@@ -1,52 +1,56 @@
 import React, { useEffect, useRef, useState } from "react";
 import GlobalApi from "../Services/GlobalApi";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
+
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 const screenWidth = window.innerWidth;
+
 function Slider() {
   const [movieList, setMovieList] = useState([]);
   const elementRef = useRef();
+
   useEffect(() => {
     getTrendingMovies();
   }, []);
 
   const getTrendingMovies = () => {
-    GlobalApi.getTrendingVideos.then((resp) => {
+    GlobalApi.getTrendingVideos().then((resp) => {
       console.log(resp.data.results);
       setMovieList(resp.data.results);
+    }).catch(error => {
+      console.error("Error fetching trending movies:", error);
     });
   };
 
-  const sliderRight = (element) => {
-    element.scrollLeft += screenWidth - 1100;
+  const sliderRight = () => {
+    elementRef.current.scrollLeft += screenWidth - 1100;
   };
-  const sliderLeft = (element) => {
-    element.scrollLeft -= screenWidth - 110;
+
+  const sliderLeft = () => {
+    elementRef.current.scrollLeft -= screenWidth - 110;
   };
+
   return (
     <div>
       <HiChevronLeft
-        className="hidden md:block text-white text-[30px] absolute
-        mx-8 mt-[150px] cursor-pointer "
-        onClick={() => sliderLeft(elementRef.current)}
+        className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer"
+        onClick={sliderLeft}
       />
       <HiChevronRight
-        className="hidden md:block text-white text-[30px] absolute
-        mx-8 mt-[150px] cursor-pointer right-0"
-        onClick={() => sliderRight(elementRef.current)}
+        className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0"
+        onClick={sliderRight}
       />
 
       <div
-        className="flex overflow-x-auto w-full px-16 py-4
-    scrollbar-none scroll-smooth"
+        className="flex overflow-x-auto w-full px-16 py-4 scrollbar-none scroll-smooth"
         ref={elementRef}
       >
         {movieList.map((item) => (
           <img
-            
-            className="min-w-full  md:h-[310px] object-cover
-            object-left-top mr-5 rounded-md hover:border-[4px]
-            border-gray-400 transition-all duration-100 ease-in"
+            key={item.id}
+            src={IMAGE_BASE_URL + item.backdrop_path}
+            alt={item.title}
+            className="min-w-full md:h-[310px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400 transition-all duration-100 ease-in"
           />
         ))}
       </div>
@@ -55,4 +59,3 @@ function Slider() {
 }
 
 export default Slider;
-//src={IMAGE_BASE_URL + item.backdrop_path}
